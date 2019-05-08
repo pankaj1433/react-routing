14593900c5f66e9d87f7f925b40e9a90a1d36d90
pankaj <pankaj.malhotra@freecharge.com>
2019-05-08 09:52:04 +0530

    props example for formated date

---
+++ src/components/FormattedDate/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const FormattedDate = props => {
+    return <h2>It is {props.date.toLocaleTimeString()}.</h2>;
+}
+
+export default FormattedDate;
--- src/components/clock/index.js
+++ src/components/clock/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+//Custom component.
+import FormattedDate from '../FormattedDate';
+
 class Clock extends React.Component {
     constructor(props) {
         super(props);
@@ -24,10 +27,11 @@ class Clock extends React.Component {
     }
 
     render() {
+        const {date} = this.state; 
         return (
             <div>
                 <h1>Hello, world!</h1>
-                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+                <FormattedDate date={date}/>
             </div>
         );
     }
